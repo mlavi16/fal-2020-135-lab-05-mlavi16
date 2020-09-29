7f58b93d73b1d93bcd4a717dcf9a58c1ea4203fe
mlavi16 <Maya.lavi11@gmail.com>
2020-09-29 17:05:48 -0400

    isDivisible and isPrime tests

--- tests.cpp
+++ tests.cpp
@@ -2,4 +2,29 @@
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+TEST_CASE("isDivisibleBy(numerator, denominator)"){
+    CHECK(isDivisibleBy(100, 25) == true);
+    CHECK(isDivisibleBy(35, 7) == false);
+    CHECK(isDivisibleBy(7, 0) == false);
+    CHECK(isDivisibleBy(-18, 9) == true);
+}
+
+TEST_CASE("isPrime(integer)"){
+    //base cases
+    CHECK(isPrime(-1) == false); //does this go here??
+    CHECK(isPrime(0) == false);
+    CHECK(isPrime(1) == false);
+    CHECK(isPrime(2) == true);
+    //small numbers
+    CHECK(isPrime(3) == true);
+    CHECK(isPrime(4) == false);
+    CHECK(isPrime(29) == true);
+    CHECK(isPrime(33) == false);
+    //large numbers
+    CHECK(isPrime(15485653) == true);
+    CHECK(isPrime(19685752) == false);
+}
+
+TEST_CASE("nextPrime(integer)"){
+    CHECK();
+}
